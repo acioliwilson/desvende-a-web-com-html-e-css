38c7971b51fb725bf349aa1078fb8c16b3fc7a7d
Wilson Acioli <acioliwilson@gmail.com>
2024-07-03 18:53:46 -0300

    animations

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,21 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8">
-                <img src="@/assets/animated-title.gif" class="main-image">
-                <p class="intro" style="padding-left: 30px;">
+                <!-- Starting Animated Title -->
+                <div class="wrapper">
+                    <img src="@/assets/tag.png" class="tag">
+                    <div class="text-content">
+                        <h1 class="headline-1">Desvende a Web</h1>
+                        <h2 class="headline-2">Com HTML &amp; CSS!</h2>
+                        <h3 class="headline-3">Em até 12x de R$15,09</h3>
+                    </div>
+                </div>
+                <!-- Ending Animated Title -->
+                <p class="intro">
                     Aprenda as linguagens HTML e CSS do zero e crie sites incríveis sem precisar saber programar!
                 </p>
-                <h2 class="neo-sans-yellow-title" style="padding-left: 30px;">Inscreva-se agora e garanta:</h2>
-                <ul class="items-list" style="padding-left: 30px;">
+                <h2 class="neo-sans-yellow-title appear">Inscreva-se agora e garanta:</h2>
+                <ul class="items-list">
                     <li class="item">
                         <i class="bi bi-check-circle-fill"></i>
                         <span>Desconto especial de lançamento: de R$ 497,00 por apenas R$ 197,00!</span>
@@ -29,7 +38,14 @@
                 </ul>
             </div>
             <div class="col-md-4">
-                <img src="@/assets/html-and-css.gif" class="ilustrative">
+                <div class="wrapper-html-and-css">
+                    <div class="shape-background">
+                        <img src="@/assets/waves-outline.png" class="waves-outline">
+                        <img src="@/assets/waves-fill.png" class="waves-fill">
+                        <img src="@/assets/html5.png" class="html5">
+                        <img src="@/assets/css3.png" class="css3">
+                    </div>
+                </div>
                 <button type="button" class="cta-btn" @click="redirectToPayment">INSCREVA-SE AGORA</button>
             </div>
         </div>
@@ -37,44 +53,52 @@
 </header>
 <!-- Ending First Section -->
 
-<img src="@/assets/separator.png" class="separator">
+<img src="@/assets/separator.png" class="separator appear">
 
 <!-- Starting Second Section -->
 <section class="why">
-    <div class="container">
-        <div class="flex">
+    <div class="left-clouds">
+        <img src="@/assets/left-clouds/back-cloud.png" class="cloud-back popUp">
+        <img src="@/assets/left-clouds/front-cloud.png" class="cloud-front popUp">
+    </div>
+    <div class="container py-5">
+        <div class="flex d-flex flex-column gap-2 align-items-center">
             <h2>
-                <span class="medium-weight d-block">Por que escolher o curso</span>
-                <span class="black-weight">Desvende a Web com HTML &amp; CSS?</span>
+                <span class="medium-weight d-block appear">Por que escolher o curso</span>
+                <span class="black-weight appear">Desvende a Web com HTML &amp; CSS?</span>
             </h2>
-            <div class="cards">
-                <div class="card-item">
+            <div class="cards-content">
+                <div class="card-item appear">
                     <i class="bi bi-star-fill"></i>
                     <p>Curso completo para iniciantes: Aprenda tudo o que precisa saber para dominar HTML e CSS, desde as bases até técnicas avançadas.</p>
                 </div>
-                <div class="card-item">
+                <div class="card-item appear">
                     <i class="bi bi-star-fill"></i>
                     <p>Aulas em vídeo e exercícios práticos: Aprenda em seu próprio ritmo e consolide seu conhecimento com exercícios práticos.</p>
                 </div>
-                <div class="card-item">
+                <div class="card-item appear">
                     <i class="bi bi-star-fill"></i>
                     <p>Suporte dedicado: Tire suas dúvidas e obtenha ajuda sempre que precisar com nossa equipe de suporte.</p>
                 </div>
-                <div class="card-item">
+                <div class="card-item appear">
                     <i class="bi bi-star-fill"></i>
                     <p>Comunidade vibrante: Junte-se à nossa comunidade online de alunos e profissionais de web design para trocar ideias e experiências.</p>
                 </div>
-                <div class="card-item">
+                <div class="card-item appear">
                     <i class="bi bi-star-fill"></i>
                     <p>Acesso vitalício: Tenha acesso ilimitado às aulas e materiais do curso para sempre.</p>
                 </div>
-                <div class="card-item">
+                <div class="card-item appear">
                     <i class="bi bi-star-fill"></i>
                     <p>Certificado de conclusão: Comprovante de suas habilidades recém-adquiridas para impressionar futuros empregadores ou clientes.</p>
                 </div>
             </div>
-            <button type="button" class="clouds-cta-btn" @click="redirectToPayment">INSCREVA-SE AGORA</button>
+            <button type="button" class="clouds-cta-btn appear" @click="redirectToPayment">INSCREVA-SE AGORA</button>
         </div>
+    </div>
+    <div class="right-clouds">
+        <img src="@/assets/right-clouds/back-cloud.png" class="cloud-back popUp">
+        <img src="@/assets/right-clouds/front-cloud.png" class="cloud-front popUp">
     </div>
 </section>
 <!-- /Endind Second Section -->
@@ -82,8 +106,8 @@
 <!-- Starting FAQ's -->
 <section class="faq py-5">
     <div class="container mt-4">
-        <h2 class="text-center neo-sans-title">Perguntas frequentes</h2>
-        <div class="dropdown-card" v-for="(faq, index) in faqs" :key="faq.id" @click="toggleDropdown(index)">
+        <h2 class="text-center neo-sans-title appear">Perguntas frequentes</h2>
+        <div class="dropdown-card appear" v-for="(faq, index) in faqs" :key="faq.id" @click="toggleDropdown(index)">
             <i class="bi bi-chevron-down" :class="{ 'bi-chevron-up': faq.isActived }"></i>
             <div class="content">
                 <p class="info-title">{{ faq.question }}</p>
@@ -97,15 +121,15 @@
 <!-- Starting Last Section -->
 <section class="last-section">
     <div class="container">
-        <img src="@/assets/separator.png" class="mx-auto d-table">
-        <h3 class="neo-sans-yellow-section-title">
+        <img src="@/assets/separator.png" class="mx-auto d-table appear">
+        <h3 class="neo-sans-yellow-section-title appear">
             Não deixe essa <br />
             oportunidade passar!
         </h3>
-        <p class="last-section-text">
+        <p class="last-section-text appear">
             Inscreva-se agora e comece sua jornada como web designer de sucesso!
         </p>
-        <button type="button" class="last-section-cta" @click="redirectToPayment">
+        <button type="button" class="last-section-cta appear" @click="redirectToPayment">
             INSCREVA-SE AGORA
         </button>
     </div>
@@ -116,14 +140,14 @@
 <footer>
     <div class="container">
         <div class="footer">
-            <div class="company-info">
+            <div class="company-info appear">
                 <a href="https://www.woxiee.vercel.app" target="_blank" rel="noopener">
                     <img src="@/assets/logo.png" alt="Woxiee">
                 </a>
                 <a href="mailto:suporte@woxiee.com" target="_blank" rel="noopener" class="emailaddress">suporte@woxiee.com</a>
                 <span class="cnpj">41.562.138/0001-61</span>
             </div>
-            <div class="social-media">
+            <div class="social-media appear">
                 <h3>Acompanhe</h3>
                 <div class="social-items">
                     <a href="#" target="_blank" rel="noopener" class="social-link">
@@ -236,11 +260,11 @@ header {
 }
 
 p.intro {
-    font-size: 20px;
+    font-size: 1.2rem;
     font-family: 'Open Sans', sans-serif;
-    font-weight: 600;
+    font-weight: 500;
     line-height: 1.7;
-    margin-top: 40px;
+    margin-top: 5rem;
 }
 
 .ilustrative {
@@ -261,15 +285,9 @@ p.intro {
 .why {
     width: 100%;
     min-height: 100vh;
-    background: url("@/assets/clouds-background.png") no-repeat;
-    background-position: center bottom;
-}
-
-.flex {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 100px 0;
+    display: table;
+    background-image: linear-gradient(to bottom, #141933, #232a4e);
+    position: relative;
 }
 
 .flex h2 {
@@ -361,11 +379,12 @@ li.item i {
     }
 }
 
-.cards {
+.cards-content {
     display: flex;
     flex-direction: column;
-    gap: 15px;
+    row-gap: 15px;
     margin-top: 40px;
+    transition: .3s ease all;
 }
 
 .card-item {
@@ -401,9 +420,10 @@ li.item i {
     padding: 13px 40px;
     color: #FFF;
     display: table;
-    margin-top: 30px;
+    margin: 30px auto;
     box-shadow: 2px 2px 30px rgba(38, 216, 255, .2);
     animation: 1s fadeInClouds ease infinite;
+    cursor: pointer !important;
 }
 
 @keyframes fadeInClouds {
@@ -618,4 +638,395 @@ p.info-text {
         gap: 20px;
     }
   }
+
+/*
+    Starting Animated Header Title
+*/
+.wrapper {
+    width: 100%;
+    max-width: 600px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    position: relative;
+    margin-top: 4rem;
+}
+
+@media screen and (max-width: 700px) {
+    .wrapper {
+        margin-top: 50px !important;
+    }
+}
+
+.tag {
+    position: absolute;
+    z-index: -1;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    opacity: .3;
+}
+
+.text-content {
+    display: table;
+    width: 100%;
+    position: relative;
+}
+
+h1.headline-1 {
+    color: #FFF;
+    font-size: 2rem;
+    font-family: 'Neo Sans Std', sans-serif;
+    letter-spacing: 1px;
+    text-shadow: 2px 2px 0 #420e75;
+    font-weight: 700;
+    text-transform: uppercase;
+    margin: 0 auto;
+    padding: 10px 35px;
+    background: #FFC32B;
+    border-radius: 10px;
+    display: table;
+    line-height: 1.2;
+    text-align: center;
+    box-shadow: 2px 0 30px rgba(0, 0, 0, .2);
+    position: relative;
+    opacity: 0;
+    transform: skew(-9deg) rotate(-5deg);
+    z-index: 2;
+    animation: 1s swipeInTop ease forwards;
+    animation-delay: 1s;
+}
+
+h2.headline-2 {
+    color: #FFF;
+    font-size: 1.8rem;
+    font-family: 'Neo Sans Std', sans-serif;
+    letter-spacing: 1px;
+    text-shadow: 1.5px 1.5px 0 #ac0a89;
+    font-weight: 700;
+    text-transform: uppercase;
+    margin: 0 auto;
+    padding: 10px 35px;
+    background: #952BFF;
+    border-radius: 10px;
+    display: table;
+    line-height: 1.2;
+    text-align: center;
+    box-shadow: 2px 0 30px rgba(0, 0, 0, .2);
+    position: relative;
+    transform: translateY(-8px) skew(-9deg) rotate(-5deg);
+    opacity: 0;
+    animation: 1s swipeInTop2 ease forwards;
+    animation-delay: 1.1s;
+    z-index: 3;
+}
+
+h3.headline-3 {
+    color: #FFF;
+    font-size: 1.8rem;
+    font-family: 'Neo Sans Std', sans-serif;
+    letter-spacing: 1px;
+    text-shadow: 1.5px 1.5px 0 #086c83;
+    font-weight: 500;
+    text-transform: uppercase;
+    margin: 0 auto;
+    padding: 20px 35px;
+    background: #26D8FF;
+    border-radius: 900px;
+    display: table;
+    line-height: 1.2;
+    text-align: center;
+    box-shadow: 2px 0 30px rgba(0, 0, 0, .2);
+    position: relative;
+    opacity: 0;
+    transform: translateY(-12px) skew(-9deg) rotate(-5deg);
+    animation: 1s swipeInTop3 ease forwards;
+    animation-delay: 1.2s;
+    z-index: 3;
+}
+
+h3.headline-3:before {
+    content: '';
+    position: absolute;
+    width: calc(100% + 9px);
+    height: calc(100% + 9px);
+    border-radius: 900px;
+    border: 1.4px solid #55e0ff;
+    box-shadow: 0 0 15px rgba(85, 224, 255, .4);
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    animation: 1s neon linear infinite;
+}
+
+@keyframes swipeInTop {
+    from { transform: translateY(10px) skew(-9deg) rotate(-5deg); opacity: .3; }
+    to { transform: translateY(0) skew(-9deg) rotate(-5deg); opacity: 1; }
+}
+
+@keyframes swipeInTop2 {
+    from { transform: translateY(5px) skew(-9deg) rotate(-5deg); opacity: .3; }
+    to { transform: translateY(-8px) skew(-9deg) rotate(-5deg); opacity: 1; }
+}
+
+@keyframes swipeInTop3 {
+    from { transform: translateY(12px) skew(-9deg) rotate(-5deg); opacity: .3; }
+    to { transform: translateY(-12px) skew(-9deg) rotate(-5deg); opacity: 1; }
+}
+
+@keyframes neon {
+    0% { opacity: 0; }
+    3%, 12% { opacity: 1; }
+    13% { opacity: 0; }
+    14% { opacity: 1; }
+    15% { opacity: 0; }
+    16% { opacity: 1; }
+    17%, 25% { opacity: 0; }
+    26%, 45% { opacity: 1; }
+    46% { opacity: 0; }
+    47% { opacity: 1; }
+    48% { opacity: 0; }
+    49%, 66% { opacity: 1; }
+    67%, 75% { opacity: 0; }
+    76% { opacity: 1; }
+    77% { opacity: 0; }
+    78%, 90% { opacity: 1; }
+    91%, 96% { opacity: 0; }
+    97%, 100% { opacity: 1; }
+}
+/*
+    Ending Animated Header Title
+*/
+
+/*
+    Starting HTML & CSS Icons Animated
+*/
+.wrapper-html-and-css {
+    width: 100%;
+    max-width: 600px;
+    min-height: 600px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    border-radius: 25px;
+    position: relative;
+}
+
+.shape-background {
+    width: 100%;
+    max-width: 350px;
+    height: 480px;
+    background: #303969;
+    border-radius: 25px;
+    box-shadow: 3px 3px 30px rgba(0, 0, 0, .2);
+    display: table;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    transition: .3s ease all;
+    animation: 3s bounceIn ease infinite;
+    z-index: 10 !important;
+}
+
+.shape-background::before {
+    content: '';
+    width: calc(100% + 14px);
+    height: calc(100% + 16px);
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    border: .4px solid #952AFF;
+    border-radius: 25px;
+    animation: 1.5s pulse ease infinite;
+    z-index: -2 !important;
+}
+
+.shape-background::after {
+    content: '';
+    width: calc(100% + 8px);
+    height: calc(100% + 8px);
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    border: .4px solid #952AFF;
+    border-radius: 25px;
+    animation: 1.5s pulse ease infinite;
+    animation-delay: .4s;
+    z-index: -2 !important;
+}
+
+.shape-background img {
+    position: absolute;
+}
+
+.shape-background img.waves-outline, .shape-background img.waves-fill {
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 85%;
+    height: auto;
+    animation: 1s rotation ease infinite;
+}
+
+.shape-background img.waves-fill {
+    animation-delay: .4s;
+}
+
+.html5 {
+    bottom: -4rem;
+    right: -3rem;
+    transform: rotate(15deg) scale(.9);
+    animation: 3s htmlBalance ease infinite;
+}
+
+.css3 {
+    top: -5rem;
+    left: -5rem;
+    transform: rotate(-18deg) scale(1);
+    animation: 3s cssBalanceMobile ease infinite;
+    animation-delay: .4s;
+}
+
+@media screen and (max-width: 600px) {
+    .wrapper-html-and-css {
+        margin-top: 70px;
+    }
+    .html5 {
+        bottom: -5rem;
+        right: -2rem;
+        transform: rotate(15deg) scale(.8);
+    }
+
+    .css3 {
+        top: -4rem;
+        left: -4.5rem;
+        transform: rotate(-18deg) scale(.9);
+    }
+}
+
+@keyframes bounceIn {
+    0%, 100% { transform: skew(4deg, 2deg) rotate(3deg) scale(1) translate(-50%, -50%); }
+    50% { transform: skew(0deg, 0deg) rotate(0deg) scale(1.03) translate(-50%, -50%); }
+}
+
+@keyframes htmlBalance {
+    0%, 100% { transform: rotate(15deg) scale(.9) translateY(0);  }
+    50% { transform: rotate(15deg) scale(.9) translateY(-2rem) translateX(-.2rem) rotate(-3deg); }
+}
+@keyframes htmlBalanceMobile {
+    0%, 100% { transform: rotate(15deg) scale(.8) translateY(0); }
+    50% { transform: rotate(15deg) scale(.8) translateY(-2rem) translateX(-.5rem) rotate(-1deg); }
+}
+
+@keyframes cssBalance {
+    0%, 100% { transform: rotate(15deg) scale(.9) translateY(0);  }
+    50% { transform: rotate(15deg) scale(.9) translateY(-2rem) translateX(-.5rem) rotate(-3deg); }
+}
+@keyframes cssBalanceMobile {
+    0%, 100% { transform: rotate(-18deg) scale(.9) translateY(0); }
+    50% { transform: rotate(-18deg) scale(.9) translateY(-2rem) translateX(-.5rem) rotate(1deg); }
+}
+
+@keyframes rotation {
+    0%, 100% { transform: scale(1) translate(-50%, -50%); }
+    50% { transform: scale(1.03) translate(-50%, -50%); }
+}
+
+@keyframes pulse {
+    0% { opacity: 0; transform: translate(-50%, -50%) scale(.5); }
+    50% { opacity: 1; transform: translate(-50%, -50%) scale(1.02); }
+    70% { opacity: .5; transform: translate(-50%, -50%) scale(1.05); }
+    100% { opacity: 0; transform: translate(-50%, -50%) scale(1.08); }
+}
+/*
+    Ending HTML & CSS Icons Animated
+*/
+
+/*
+    Starting Scroll Animation
+*/
+.appear {
+    animation: appear linear;
+    animation-timeline: view();
+    animation-range: entry 0% cover 40%;
+}
+@keyframes appear {
+    from {
+        opacity: 0;
+        transform: translateX(-100px);
+    }
+    to {
+        opacity: 1;
+        transform: translateX(0px);
+    }
+}
+
+.popUp {
+    animation: popUp linear;
+    animation-timeline: view();
+    animation-range: entry 0% cover 40%;
+}
+@keyframes popUp {
+    0% {
+        opacity: 0;
+        transform: scale(0);
+    }
+    80% {
+        opacity: 1;
+        transform: scale(1.05);
+    }
+    90% {
+        opacity: 1;
+        transform: scale(.9);
+    }
+    100% {
+        opacity: 1;
+        transform: scale(1);
+    }
+}
+/*
+    Ending Scroll Animation
+*/
+
+.left-clouds {
+    width: 100%;
+    display: table;
+    position: absolute;
+    bottom: 0;
+    overflow: hidden;
+}
+.left-clouds img.cloud-back {
+    position: absolute;
+    left: -12rem;
+    bottom: -10rem;
+}
+.left-clouds img.cloud-front {
+    position: absolute;
+    left: -12rem;
+    bottom: -10rem;
+}
+
+.right-clouds {
+    width: 100%;
+    display: table;
+    position: absolute;
+    bottom: 0;
+    overflow: hidden;
+}
+.right-clouds img.cloud-back {
+    position: absolute;
+    right: -12rem;
+    bottom: -10rem;
+}
+.right-clouds img.cloud-front {
+    position: absolute;
+    right: -12rem;
+    bottom: -10rem;
+}
+
 </style>
